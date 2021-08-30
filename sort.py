55db47a49c0ebefcd1b971da2b10f7e1368d540e
Don Vu <33820836+Don-Vu@users.noreply.github.com>
2021-08-30 10:00:56 -0600

    Update sort.py

--- sort.py
+++ sort.py
@@ -3,9 +3,8 @@ import sys
 
 def selection_sort(number_list):
     '''An implementation of selection sort -
-    The first loop iterates through all indexes of the list.
-    The second loop iterates through all indexes from the current index to the last index.
-    The number at the current index is then swapped with the next lowest number.
+    For every element in a list, looks for the smallest number
+    between the i'th and last element, then swaps the two values
     '''
     length = len(number_list)
     for index in range(length):
@@ -14,6 +13,26 @@ def selection_sort(number_list):
             if number_list[ith_index] < number_list[lowest_index]:
                 lowest_index = ith_index
         number_list[lowest_index], number_list[index] = number_list[index], number_list[lowest_index]
+    return number_list
+
+
+def insertion_sort(number_list):
+    '''An implementation of insertion sort -
+    Takes the next unsorted number in an array, and inserts it
+    into its sorted subarray, beginning at the front of the array
+    '''
+    length = len(number_list)
+    for count, number in enumerate(number_list):
+        if count == length - 1:
+            break
+        next_number = number_list[count + 1]
+        if next_number < number:
+            lowest_index = count
+            for count_2, number_2 in reversed(list(enumerate(number_list[:count]))):
+                if number_2 < number_list[lowest_index] and number_2 > next_number:
+                    lowest_index = count_2
+            number_list.pop(count + 1)
+            number_list.insert(lowest_index, next_number)
     return number_list
 
 
@@ -34,7 +53,7 @@ def main():
     numbers = []
     populate_list(numbers)
 
-    numbers = selection_sort(numbers)
+    numbers = insertion_sort(numbers)
     print(numbers)
 
 main()
